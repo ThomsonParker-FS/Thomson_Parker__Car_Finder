e4a7d087c39e9571b9a4b353697a754d878ebbd8
ThomsonParker-FS <pcthomson@student.fullsail.edu>
2025-06-08 18:09:21 -0400

    filter works, organizing code required

--- src/index.js
+++ src/index.js
@@ -10,8 +10,14 @@ const section = document.querySelector("section");
 
 //year make and model options
 
-const yearSet = new Set();
+const initSelections = (selector) => {
+  let optionSelect = document.createElement("option");
+  optionSelect.value = "select";
+  optionSelect.text = "select";
+  selector.appendChild(optionSelect);
+};
 
+const yearSet = new Set();
 
 //make set and model set are dependent on year.
 const makeSet = new Set();
@@ -20,40 +26,34 @@ const modelSet = new Set();
 //when year is selected, create make set from all values of year
 //when make set is selected, create all models from make.
 
-
 //create sets for the data for the dropdowns.
 
 const populateSets = () => {
   dataset.forEach((i) => {
     yearSet.add(i.year);
   });
-
-
-  //needs to be filtered on the spot
-  // dataset.forEach((i) => {
-  //   makeSet.add(i.Manufacturer);
-  // });
-
-  // dataset.forEach((i) => {
-  //   modelSet.add(i.model);
-  // });
 };
 
+/** I like to use docs to keep track of everything
+ *
+ *Originally had strict equality ===, but changed it to == which fixed it not working.
+ *
+ * @param {string} year
+ * @returns all entries where the year matches the selection.
+ */
 const filterByYear = (year) => {
   return dataset.filter((i) => {
-    return i.year === year;
+    return i.year == year;
   });
 };
 
-const filterByModel = (model) => {
+const filterByMake = (make) => {
   return dataset.filter((i) => {
-    return i.model === model;
+    return i.Manufacturer == make;
   });
 };
 
 //initial state is blank
-
-
 
 //in order to sort you need to populate an array, sort the array, and put it back in the set.
 
@@ -66,7 +66,6 @@ const sortSet = (set, order) => {
   arr.sort((a, b) => {
     return a > b ? -1 * order : 1 * order;
   });
-  console.log(arr);
   //clear the set and re-add items in sorted order.
   set.clear();
   arr.forEach((item) => {
@@ -89,40 +88,81 @@ const optionsPopulate = (set, selector) => {
   });
 };
 
+selectYear.addEventListener("change", yearHandler);
+selectMake.addEventListener("change", makeHandler);
+selectModel.addEventListener("change", modelHandler);
+
+/**Handles when year is selected and or changed
+ *
+ * @param {event} event
+ */
+function yearHandler(e) {
+  //clear the models when this changes
+  clearOptions(modelSet, selectModel);
+  clearOptions(makeSet, selectMake);
+
+  let yearRaw = filterByYear(selectYear.value);
+
+  yearRaw.forEach((item) => {
+    makeSet.add(item.Manufacturer);
+  });
+
+  sortSet(makeSet, -1);
+  optionsPopulate(makeSet, selectMake);
+}
+
+/** Handles when make is selected and or changed
+ *
+ * @param {event} event
+ * @returns
+ */
+function makeHandler(e) {
+  //clear the models when this changes
+  clearOptions(modelSet, selectModel);
+
+  let makeRaw = filterByMake(selectMake.value);
+  makeRaw.forEach((item) => {
+    modelSet.add(item.model);
+  });
+  sortSet(modelSet, -1);
+  optionsPopulate(modelSet, selectModel);
+}
+
+function modelHandler(e){
+  let finalYear = selectYear.value;
+  let finalMake = selectMake.value;
+  let finalModel = selectModel.value;
+
+  //filter by those attributes
+  let finalResult = dataset.find((item) => {
+    return item.year == finalYear && item.Manufacturer == finalMake && item.model == finalModel;
+  });
+
+  console.log(finalResult);
+}
+
+function clearOptions(set, selector){
+  set.clear();
+  //quick way to remove all children from the dropdown, thank you internet.
+  selector.innerHTML = "";
+}
+
+
+
 (() => {
-  // populateSets();
-  // sortSet(yearSet, 1);
+  populateSets();
+  sortSet(yearSet, 1);
   // sortSet(modelSet, -1);
   // sortSet(makeSet, -1);
-  // optionsPopulate(yearSet, selectYear);
+  optionsPopulate(yearSet, selectYear);
+
+  //after this we sort by year, then make and then model.
+
   // optionsPopulate(makeSet, selectMake);
   // optionsPopulate(modelSet, selectModel);
-  // section.appendChild(selectYear);
-  // section.appendChild(selectMake);
-  // section.appendChild(selectModel);
+  initSelections(selectModel);
+  //initSelections(selectMake);
+  section.appendChild(selectYear);
+  section.appendChild(selectMake);
+  section.appendChild(selectModel);
 })();
-
-//populateSets();
-
-//Options are populated
-//optionsPopulate(yearSet, selectYear);
-// optionsPopulate(makeSet, selectMake);
-// optionsPopulate(modelSet, selectModel);
-
-// let optionA = document.createElement("option");
-// optionA.value = "select";
-// optionA.text = "select";
-// selectMake.appendChild(optionA);
-// section.appendChild(selectMake);
-
-//add the selectors to the dom.
-//section.appendChild(selectMake);
-//console.log(selectMake);
-// section.appendChild(selectYear);
-// section.appendChild(selectModel);
-
-//read the data from the selected options.
-
-// let currentMakeSelection = selectMake.value;
-// let currentModelSelection = selectModel.value;
-// let currentYearSelection = selectYear.value;
